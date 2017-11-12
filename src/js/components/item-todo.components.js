df7598d0443332da09818852e7ade486b6a6a96c
Tsega <31296401+tsegishey@users.noreply.github.com>
2017-11-11 18:30:23 -0800

    Add files via upload
    communicate parent and child components

--- src/js/components/item-todo.components.js
+++ src/js/components/item-todo.components.js
@@ -3,24 +3,33 @@ class ItemTodosctrl
 {
   constructor(CourseService) {
     'ngInject';
-   this._CourseService= CourseService;
+  // this._CourseService= CourseService;
    
       }
-  $onInit(){
-        this._CourseService.postItemList().then(res => {
-          console.log(res.data.postListDone);
-              this.items=res.data.postListDone;
-          });
+  // $onInit(){
+  //       this._CourseService.postItemList().then(res => {
+  //         console.log(res.data.postListDone);
+  //             this.items=res.data.postListDone;
+  //         });
         
-      }
-}
+  //     }
+  onEditPost(post){
+   this.onEdit=post;
+   this.navigationCtrl.updatePage(this.onEdit,);
+   console.log(this.onEdit);
+  }
+ }
 
 let ItemTodos= {
   bindings: {
-    items: '='
+    items: '<',
+    onEdit:'<'
   },
   templateUrl: 'components/item-todo.html',
-  controller:ItemTodosctrl
+  controller:ItemTodosctrl,
+  require:{
+    navigationCtrl:'^navigationComp'
+  }
 };
 
 export default ItemTodos;
